39c096945e0a277a48e7145a69d2c5418e6dbfe8
Alfiansyah Aresta <alfiansyah.aresta@elga.net.id>
2023-10-10 17:16:45 +0700

    tb1_op_2

--- lib/form_login.dart
+++ lib/form_login.dart
@@ -8,53 +8,117 @@ class LoginPage extends StatelessWidget {
   final TextEditingController passwordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Login'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             TextField(
+//               controller: usernameController,
+//               decoration: InputDecoration(labelText: 'Username'),
+//               style: TextStyle(
+//               fontSize: 18.0, 
+//               fontWeight: FontWeight.bold, 
+//               ),
+//             ),
+//             TextField(
+//               controller: passwordController,
+//               decoration: InputDecoration(labelText: 'Password'),
+//               obscureText: true,
+//               style: TextStyle(
+//               fontSize: 18.0, 
+//               fontWeight: FontWeight.bold, 
+//               ),
+//             ),
+//             SizedBox(height: 50.0),
+//             ElevatedButton(
+//               onPressed: () {
+//                 // Cek Login
+//                 String username = usernameController.text;
+//                 String password = passwordController.text;
+//                 if (username == 'Alfiansyah Aresta' && password == '0030') {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(builder: (context) => PageProduct()),
+//                   );
+//                 } else {
+//                   // Tampilkan Pesan Kesalahan Jika Login Gagal
+//                   ScaffoldMessenger.of(context).showSnackBar(
+//                     SnackBar(
+//                       content: Text('Login gagal. Coba lagi.'),
+//                     ),
+//                   );
+//                 }
+//               },
+//               child: Text('Login'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text('LOGIN'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
-              style: TextStyle(
-              fontSize: 18.0, 
-              fontWeight: FontWeight.bold, 
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: TextField(
+                controller: usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              style: TextStyle(
-              fontSize: 18.0, 
-              fontWeight: FontWeight.bold, 
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
             SizedBox(height: 50.0),
-            ElevatedButton(
-              onPressed: () {
-                // Cek Login
-                String username = usernameController.text;
-                String password = passwordController.text;
-                if (username == 'Alfiansyah Aresta' && password == '0030') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => PageProduct()),
-                  );
-                } else {
-                  // Tampilkan Pesan Kesalahan Jika Login Gagal
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Login gagal. Coba lagi.'),
-                    ),
-                  );
-                }
-              },
-              child: Text('Login'),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Cek Login
+                  String username = usernameController.text;
+                  String password = passwordController.text;
+                  if (username == 'Alfiansyah Aresta' && password == '0030') {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PageProduct()),
+                    );
+                  } else {
+                    // Tampilkan Pesan Kesalahan Jika Login Gagal
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Login Gagal, Cobalah Kembali.'),
+                      ),
+                    );
+                  }
+                },
+                child: Text('LOGIN'),
+              ),
             ),
           ],
         ),
